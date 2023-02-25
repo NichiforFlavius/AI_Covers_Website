2eda12aa21adbf9e82d538323f582f18cb00ff14
Flavius Nichifor <flavius_nichifor@yahoo.com>
2023-02-25 20:59:17 +0200

    tested UI with four album cover images

--- Website_App/ai_covers/src/App.js
+++ Website_App/ai_covers/src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
+
+import v1 from './assets/v2_album_cover_70.jpg'
+import v2 from './assets/v2_album_cover_241.jpg'
+import v3 from './assets/v2_album_cover_294.jpg'
+import v4 from './assets/v2_album_cover_323.jpg'
+
 import './App.css';
 
 function App() {
     return ( 
         <div className = "App" >
             <header className = "App-header" >
-            <img src = { logo } className = "App-logo" alt = "logo" />
-            <p> Edit <code> src / App.js </code> and save to reload. </p>
-            <a className = "App-link" href = "https://reactjs.org" target = "_blank" rel = "noopener noreferrer" > Learn React </a> 
+            <h1>
+                AI generated album covers!
+            </h1>
+            <table>
+                <tr>
+                    <td>
+                        <div className="card">
+                            <img src = { v1 } alt = "logo" className="card" style={{width: "300px", padding: "2px"}}/>
+                            <div className="card-body">
+                                <h5 className="card-title" style={{color: "black"}}>Sovereignty</h5>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="card">
+                            <img src = { v2 } alt = "logo" className="card" style={{width: "300px", padding: "2px"}}/>
+                            <div className="card-body">
+                                <h5 className="card-title" style={{color: "black"}}>Chappel of the Damned</h5>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="card">
+                            <img src = { v3 } alt = "logo" className="card" style={{width: "300px", padding: "2px"}}/>
+                            <div className="card-body">
+                                <h5 className="card-title" style={{color: "black"}}>Yearn for the end</h5>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="card">
+                            <img src = { v4 } alt = "logo" className="card" style={{width: "300px", padding: "2px"}}/>
+                            <div className="card-body">
+                                <h5 className="card-title" style={{color: "black"}}>It has awakened</h5>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+
             </header> 
         </div>
     );
